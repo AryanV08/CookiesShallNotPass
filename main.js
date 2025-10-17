ad9c651c5040f4311b49239f421356adced1d83d
pengfei guo <adz9596@gmail.com>
2025-10-17 00:09:44 -0700

    added some ugly ui

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 (() => {
-    window.addEventListener("load", init);
-  
-    function init() {
-      const box = document.getElementById("toggler-1");
-  
-      box.addEventListener("change", () => {
-        if (box.checked) {
-          alert("Checkbox is checked ✅");
-        } else {
-          alert("Checkbox is unchecked ❌");
-        }
-      });
+  window.addEventListener("load", init);
+
+  async function init() {
+    const box = document.getElementById("toggler-1");
+    box.addEventListener("change", async () => {
+      if (box.checked) {
+        await showCookies();
+      } else {
+        document.getElementById("cookietable").innerHTML = "";
+        document.getElementById("cookietable").classList.remove("show");
+
+       
+      }
+    });
+  }
+
+  async function showCookies() {
+    const cookietable = document.getElementById("cookietable");
+    document.getElementById("cookietable").classList.add("show");
+
+    
+
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const url = tab.url;
+
+    if (!/^https?:\/\//i.test(url)) {
+      cookietable.innerHTML = "<p>Cookies not accessible on this page.</p>";
+      
+      return;
     }
-  })();
+
+    const cookies = await chrome.cookies.getAll({ url });
+
+   
+
+    let p = document.createElement("p")
+    p.textContent = "we have found  " + cookies.length + " cookies! "
+
+
+
+    cookietable.appendChild(p);
+    
+  }
+})();
